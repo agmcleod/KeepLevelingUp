4129b301fad4bce4efedf6631313ecf6f9ca9068
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-08 00:15:18 -0500

    create day reducer.

--- src/days/reducers.js
+++ src/days/reducers.js
@@ -1,14 +1,43 @@
 import * as ActionTypes from './day_actions';
 import {createUuid} from '../utils/utility_functions';
 
+const createSetFromPreviousDay = function(previousDaySet) {
+  return {
+    last_reps: previousDaySet.reps, weight: previousDaySet.weight, last_duration: previousDaySet.duration
+  };
+};
+
 export const days = function(state = {}, action) {
   switch (action.type) {
     case ActionTypes.CREATE_DAY:
       const {previousDay, routine} = action;
-      const newDay = {uuid: createUuid, created_at: new Date().toISOString()};
-      if (previousDay) {
-        
+      const newDay = {uuid: createUuid, created_at: new Date().toISOString(), exercises: []};
+      for (const exercise of routine.exercies) {
+        const dayExercise = {...exercise, sets: []};
+        for (let i = 0; i < exercise.sets; i++) {
+          dayExercise.sets.push({
+            weight: dayExercise.showWeight ? 0 : undefined, reps: dayExercise.reps, duration: dayExercise.duration
+          });
+        }
+
+        newDay.exercises.push(dayExercise);
       }
+      if (previousDay) {
+        for (const previousDayExercise of previousDay.exercises) {
+          const newDayEx = newDay.exercises.find((ex) => ex.uuid === previousDayExercise.uuid);
+          if (newDayEx) {
+            for (let i = 0; i < previousDayExercise.sets.length; i++) {
+              const set = newDayEx.sets[i];
+              if (set) {
+                const previousDaySet = previousDayExercise.sets[i];
+                Object.assign(set, createSetFromPreviousDay(previousDaySet, set));
+              }
+            }
+          }
+        }
+      }
+      const daysData = Object.assign({}, state.days, {[newDay.uuid]: newDay});
+      return Object.assign({}, state, {days: daysData});
     case ActionTypes.LIST_DAYS:
       return Object.assign({}, state, action.data);
     default:
--- src/routines/routine_actions.js
+++ src/routines/routine_actions.js
@@ -1,5 +1,6 @@
 import {GET_DATA} from '../middleware/get_data';
 import {SAVE_DATA} from '../middleware/save_data';
+import {createUuid} from '../utils/utility_functions';
 
 export const SET_ROUTINE = 'SET_ROUTINE';
 export const UPDATE_ROUTINE = 'UPDATE_ROUTINE';
@@ -8,6 +9,11 @@ export const DELETE_ROUTINE = 'DELETE_ROUTINE';
 
 export function setRoutine(data) {
   return (dispatch) => {
+    for (const exercise of data.exercises) {
+      if (!exercise.uuid) {
+        exercise.uuid = createUuid();
+      }
+    }
     return dispatch({type: SET_ROUTINE, data: data});
   };
 }
